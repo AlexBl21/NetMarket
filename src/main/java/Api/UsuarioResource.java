7da120c2458882cc089f871bc38ffa61d580e63c
ElMopri <alopeztriana1@gmail.com>
2023-05-30 11:25:50 -0500

    UsuarioResource terminado

--- src/main/java/Api/UsuarioResource.java
+++ src/main/java/Api/UsuarioResource.java
@@ -25,4 +25,59 @@ import java.util.List;
 @Path("/apiturista")
 public class UsuarioResource {
     
+        UsuarioDAO usuarioDAO = new UsuarioDAO();
+
+    @GET
+    @Path("/usuario")
+    @Produces(MediaType.APPLICATION_JSON)
+
+    public Response consultar() {
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios = usuarioDAO.consultar();
+        return Response
+                .status(200)
+                .entity(usuarios)
+                .build();
+    }
+
+    @GET
+    @Path("/usuario/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response consultarId(@PathParam("id") int id) {
+        Usuario usuario = new Usuario(id);
+        return Response
+                .status(200)
+                .entity(usuarioDAO.consultarPorId(usuario))
+                .build();
+    }//matias
+
+    @POST
+    @Path("/usuario")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response crear(Usuario usuario) {
+        try {
+            usuarioDAO.insertar(usuario);
+            return Response.status(Response.Status.CREATED).entity(usuario).build();
+        } catch (Exception ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(ex.getMessage()).build();
+        }
+    }
+
+    @DELETE
+    @Path("/usuario/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response borrar(@PathParam("id") int id) {
+        Usuario usuario = new Usuario(id);
+        int i = usuarioDAO.borrar(usuario);
+        if (i == 0) {
+            return Response
+                    .status(Response.Status.BAD_REQUEST)
+                    .entity("Usuario not found")
+                    .build();
+        } else {
+            return Response.ok("Correcto").build();
+        }
+    }
+    
 }
