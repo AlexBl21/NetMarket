a270128fc874d8dcafa995dfcc898029a2d30625
ElMopri <alopeztriana1@gmail.com>
2023-05-30 10:58:50 -0500

    se agrego el RestAplication y el CorsFilter(este se debe ir editando)

---
+++ src/main/java/Api/UsuarioResource.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Api;
+
+import Modelo.DAO.UsuarioDAO;
+import Modelo.Entity.Usuario;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author alope
+ */
+@Path("/apiturista")
+public class UsuarioResource {
+    
+}
